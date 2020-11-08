95cf9e0ba986c6649544228d4c55ec4c0c543fde
JamesBurch <jamesrobertburch@gmail.com>
2020-11-08 20:10:14 +0000

    Got spotify cred working

--- install.sh
+++ install.sh
@@ -5,10 +5,11 @@ echo "Starting installation script"
 sudo apt-get update
 sudo apt-get upgrade
 
+sudo apt-get install python3-pip
 sudo apt-get install python-rpi.gpio python-spidev python-pip python-pil python-numpy
 sudo pip install st7789
 
-pip install spotipy 
+pip install spotipy
 
 curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
 
