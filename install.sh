bf333daa2fe35ee919f336070df0a35961470322
JamesBurch <jamesrobertburch@gmail.com>
2020-11-08 21:23:30 +0000

    Changed to python3 versions and libatlas-base-dev to fix numpy problem

--- install.sh
+++ install.sh
@@ -5,11 +5,10 @@ echo "Starting installation script"
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install python3-pip
-sudo apt-get install python-rpi.gpio python-spidev python-pip python-pil python-numpy
+sudo apt-get install python3-rpi.gpio python3-spidev python3-pip python3-pil python3-numpy libatlas-base-dev
 sudo pip install st7789
 
-pip install numpy pillow spotipy
+pip install numpy Pillow spotipy
 
 curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
 
