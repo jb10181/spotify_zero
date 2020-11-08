245f3b80272f91a11c8a5065bf08b9fe89ac4b92
JamesBurch <jamesrobertburch@gmail.com>
2020-11-08 18:07:02 +0000

    Reset keys and replaced hard-coded keys with user inputs

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ sudo apt-get upgrade
 sudo apt-get install python-rpi.gpio python-spidev python-pip python-pil python-numpy
 sudo pip install st7789
 
+pip install spotipy 
+
 curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
 
 sudo raspi-config
