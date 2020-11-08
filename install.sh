9f614477752244234d0afdc8d9bb6563724dda9c
JamesBurch <jamesrobertburch@gmail.com>
2020-11-08 21:28:25 +0000

    Added more dependencies

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ echo "Starting installation script"
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install python3-rpi.gpio python3-spidev python3-pip python3-pil python3-numpy libatlas-base-dev
+sudo apt-get install python3-rpi.gpio python3-spidev python3-pip python3-pil python3-numpy
+sudo apt install libopenjp2-7 libopenjp2-7-dev libopenjp2-tools libatlas-base-dev
 sudo pip install st7789
 
 pip install numpy Pillow spotipy
