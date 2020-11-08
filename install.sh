ba982fffe5bf1923102d20821ec903f94d765c01
JamesBurch <jamesrobertburch@gmail.com>
2020-11-08 20:23:42 +0000

    Troubleshooting

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt-get install python3-pip
 sudo apt-get install python-rpi.gpio python-spidev python-pip python-pil python-numpy
 sudo pip install st7789
 
-pip install spotipy
+pip install spotipy pillow
 
 curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
 
