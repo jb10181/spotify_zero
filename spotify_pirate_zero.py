81520c486f4f89545bddfb044ea5aa56fcd8c096
JamesBurch <jamesrobertburch@gmail.com>
2020-11-13 10:49:04 +0000

    Troubleshooting

--- spotify_pirate_zero.py
+++ spotify_pirate_zero.py
@@ -126,15 +126,28 @@ im_album = draw_rotated_text(img, name_album, (0, 60), 0, font=font_album, fill=
 t_start = time.time()
 
 while True:
-    x = 240 - int((time.time() - t_start) * 10)
-    if x <= -240:
-        t_start = time.time()
-    draw.rectangle((0, 120, disp.width, 80), (0, 0, 0))
-    draw.text((int(0 - x), 100), name_song, font=font_song, fill=(255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    width, height = draw.textsize(name_song, font=font_song)
+    textimage = Image.new('RGBA', (width, height), (0, 0, 0, 0))
+    textdraw = ImageDraw.Draw(textimage)
+    textdraw.text((0, 0), name_song, font=font_song, fill=(255, 255, 255))
 
-    # im_song = draw_rotated_text(img, name_song, (x, 100), 0, font=font_song, fill=(255, 255, 255))
-    # im_song = im_song.transform(img.size, Image.AFFINE, (1, 0, 2, 0, 1, 0))
+    x = (time.time() - t_start) * 100
+    x %= (width + disp.width)
+    draw.rectangle((0, 0, disp.width, 80), (0, 0, 0))
+    draw.text((int(WIDTH - x), HEIGHT), name_song, font=font_song, fill=(255, 255, 255))
     disp.display(img)
+
+
+    # x = 240 - int((time.time() - t_start) * 10)
+    # if x <= -240:
+    #     t_start = time.time()
+    # draw.rectangle((0, 120, disp.width, 80), (0, 0, 0))
+    # draw.text((int(0 - x), 100), name_song, font=font_song, fill=(255, 255, 255))
+    #
+    # # im_song = draw_rotated_text(img, name_song, (x, 100), 0, font=font_song, fill=(255, 255, 255))
+    # # im_song = im_song.transform(img.size, Image.AFFINE, (1, 0, 2, 0, 1, 0))
+    # disp.display(img)
 
 # Write buffer to display hardware, must be called to make things visible on the
 # display!
