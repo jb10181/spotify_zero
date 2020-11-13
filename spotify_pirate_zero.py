760c1b51d9de10524a24f063ca7e8926fb1fbd9a
jb10181 <jburch@protonmail.com>
2020-11-13 09:42:22 +0000

    correctly centred

--- spotify_pirate_zero.py
+++ spotify_pirate_zero.py
@@ -124,7 +124,7 @@ def draw_rotated_text(image, text, position, angle, font, fill=(255, 255, 255)):
     print(position)
     print(width)
     print(height)
-    static_offset = (position[0], position[1] + int((240 - width) / 2))
+    static_offset = (position[0] + int((240 - width) / 2), position[1])
     print(static_offset)
     image.paste(rotated, static_offset, rotated)
 
